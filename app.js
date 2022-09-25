d14a9f3d99eb3a417e174bb75135234caa015f9a
ancc02 <oandresccc@gmail.com>
2022-09-25 13:37:27 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+
+app.use(session({
+    secret: '123456',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.get('/', (req, res) => {
+    req.session.usuario = 'Juan Perez';
+    req.session.rol = 'Admin';
+    req.session.visitas = req.session.visitas ? ++req.session.visitas : 1;
+
+    res.send(`El usuario <strong>${req.session.usuario}</strong> 
+    con rol <strong>${req.session.rol}</strong>
+    ha visitado esta pagina <strong>${req.session.visitas}</strong>`)
+});
+
+app.listen(3000, (req, res) => {
+    console.log('SERVER UP');
+});
